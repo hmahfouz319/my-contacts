8068015633f1a5d2a9c27c0e10e0927cadfaf0b2
hmahfouz319 <hisham.mahfouz68@gmail.com>
2023-04-30 20:08:11 +0300

    update

--- lib/my_contacts.dart
+++ lib/my_contacts.dart
@@ -1,43 +1,69 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'widgets/social_media_icon.dart';
 
 // ignore: must_be_immutable
-class MyContacts extends StatelessWidget {
+class MyContacts extends StatefulWidget {
   MyContacts({super.key});
 
+  @override
+  State<MyContacts> createState() => _MyContactsState();
+}
+
+class _MyContactsState extends State<MyContacts> {
+  String? myPlatform;
+  Uri? myUrl;
+  Uri phoneNumber = Uri.parse('tel:+96566230370');
   Map<String, String> socialMedia = {
     "facebook.png": "https://www.facebook.com/hisham.mahfouz.39",
+    // "facebook":Uri.parse("https://www.facebook.com/hisham.mahfouz.39") ,
     'linkedin.png': 'https://www.linkedin.com/feed/',
     "watsapp.png": "https://wa.me/+96566230370",
   };
 
   // List<String> socialMedia = [
-  //   "facebook.png",
-  //   // 'github.png',
-  //   // 'Instagram.png',
-  //   'linkedin.png',
-  //   // 'tiktok.png',
-  //   // 'vimeo.png',
-  //   "watsapp.png",
-  //   // 'youtube.png',
-  //   // '33.jpg'
-  // ];
-  // List<String> socialMediaLink = [
-  //   "https://www.facebook.com/hisham.mahfouz.39",
-  //   // 'github.png',
-  //   // 'Instagram.png',
-  //   'https://www.linkedin.com/feed/',
-  //   // 'tiktok.png',
-  //   // 'vimeo.png',
-  //   "https://wa.me/+96566230370",
-  //   // 'youtube.png',
-  //   // '33.jpg'
-  // ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        title: Text('My Contact Screen'),
+        leading: InkWell(
+          child: Icon(Icons.home),
+          onTap: () {
+            print('Icon is pressed');
+          },
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 8.0),
+            child: IconButton(
+              icon: myPlatform == null
+                  ? Icon(
+                      Icons.phone,
+                      size: 25,
+                      // color: Colors.grey,
+                    )
+                  : Material(
+                      color: Colors.transparent,
+                      borderRadius: BorderRadius.circular(50),
+                      clipBehavior: Clip.antiAliasWithSaveLayer,
+                      elevation: 4,
+                      child: Image(
+                          image: AssetImage(
+                            "assets/$myPlatform",
+                          ),
+                          fit: BoxFit.cover),
+                    ),
+              onPressed: () {
+                // launchUrl(Uri.parse('tel:+96566230370'));
+                myUrl == null
+                    ? launchUrl(phoneNumber)
+                    : launchUrl(myUrl!, mode: LaunchMode.externalApplication);
+              },
+            ),
+          ),
+        ],
+      ),
       backgroundColor: const Color.fromARGB(255, 3, 7, 30),
       body: SizedBox(
         width: double.infinity,
@@ -102,9 +128,31 @@ class MyContacts extends StatelessWidget {
                 gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                     crossAxisCount: 3),
                 itemBuilder: (context, index) {
-                  return SocialMediaIcon(
-                    socialMedia: socialMedia.keys.toList()[index],
-                    socialMedialink: socialMedia.values.toList()[index],
+                  return Padding(
+                    padding: const EdgeInsets.all(12),
+                    child: InkWell(
+                      
+                      child: CircleAvatar(
+                        backgroundColor: Colors.transparent,
+                        backgroundImage: AssetImage(
+                            'assets/${socialMedia.keys.toList()[index]}'),
+                        radius: 40,
+                      ),
+                      onDoubleTap: () {
+                        myPlatform = socialMedia.keys.toList()[index];
+                        myUrl = Uri.parse(socialMedia.values.toList()[index]);
+                        setState(() {});
+                      },
+                      onTap: () {
+                        // print('current value = $myPlatform');
+                        myPlatform = socialMedia.keys.toList()[index];
+                        myUrl = Uri.parse(socialMedia.values.toList()[index]);
+                        // print('current value = $myPlatform');
+                        setState(() {});
+                        launchUrl(Uri.parse(socialMedia.values.toList()[index]),
+                            mode: LaunchMode.externalApplication);
+                      },
+                    ),
                   );
                 },
                 shrinkWrap: true,
